92f94aa656a009c9f89a63574efa932bb03cc4fd
chrostine <christineswierkosz@gmail.com>
2026-04-30 10:30:40 +0200

    Added accerleration

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,12 +5,22 @@ public class PlayerControls : MonoBehaviour
     
     private Rigidbody2D rb;
     private Vector2 moveInput;
-    public float moveSpeed = 4f;
+    private Vector2 currentVelocity;
     private int powerUpCount = 0;
+    private float acceleration;
+    private float deceleration;
+
+    public float moveSpeed = 4f;
+
+    [Header("Player accerleration")]
+    public int maxPowerUps = 10; // det maksimale antal powerups du forventer at spilleren kan samle
+    public AnimationCurve accelerationCurve;
+    public AnimationCurve decelerationCurve;
 
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>();  
+      rb = GetComponent<Rigidbody2D>();
+      UpdateMovementStats();
     }
 
   //Håndterer spillerens input for bevægelse og opdaterer spillerens position i FixedUpdate for at sikre jævn bevægelse.
@@ -22,12 +32,30 @@ public class PlayerControls : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocity = moveInput * moveSpeed;
+      Vector2 targetVelocity = moveInput.normalized * moveSpeed;
+
+      // vælger acceleration eller deceleration afhængigt af om spilleren bevæger sig
+      float smoothSpeed = moveInput == Vector2.zero ? deceleration : acceleration;
+      currentVelocity = Vector2.Lerp(currentVelocity,targetVelocity,smoothSpeed);
+
+      rb.linearVelocity = currentVelocity;
     }
  //viser hvor mange powerups spilleren har samlet, og øger tælleren hver gang en powerup bliver samlet op.
     public void CollectPowerUp()
     {
       powerUpCount++;
       Debug.Log("PowerUps collected: " + powerUpCount);
+      UpdateMovementStats();
+    }
+
+// håndterer acceleration og deceleration, som er afhængig af collected Powerups.
+    private void UpdateMovementStats ()
+    {
+      float t = Mathf.Clamp01((float)powerUpCount/maxPowerUps);
+      acceleration = accelerationCurve.Evaluate(t);
+      deceleration = decelerationCurve.Evaluate(t);
+      Debug.Log("moveSpped: " + moveSpeed);
+      Debug.Log("acceleration: " + acceleration);
+      Debug.Log("deceleration: " + deceleration);
     }
 }
